5d9d9c914276d3e4b38d34437174fd60c2286b99
李富强 <lifuqiang@carystudio.com>
2018-02-03 14:30:59 +0800

    Revert "	new file:   package/base-files/files/etc/uci-defaults/button/buttons"
    This reverts commit 474e57b48bebc311b3453f4bdb2cc0520476dfd9.

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -2,10 +2,13 @@
 # Copyright (C) 2010 OpenWrt.org
 #
 
+. /lib/ramips.sh
+
 PART_NAME=firmware
+RAMFS_COPY_DATA=/lib/ramips.sh
 
 platform_check_image() {
-	local board=$(board_name)
+	local board=$(ramips_board_name)
 	local magic="$(get_magic_long "$1")"
 
 	[ "$#" -gt 1 ] && return 1
@@ -14,22 +17,20 @@ platform_check_image() {
 	3g150b|\
 	3g300m|\
 	a5-v11|\
+	ac1200pro|\
 	ai-br100|\
 	air3gii|\
 	all0239-3g|\
-	all0256n-4M|\
-	all0256n-8M|\
+	all0256n|\
 	all5002|\
 	all5003|\
 	ar725w|\
-	asl26555-8M|\
-	asl26555-16M|\
+	asl26555|\
 	awapn2403|\
-	awm002-evb-4M|\
-	awm002-evb-8M|\
+	awm002-evb|\
+	awm003-evb|\
 	bc2|\
 	broadway|\
-	c108|\
 	carambola|\
 	cf-wr800n|\
 	cs-qr10|\
@@ -52,20 +53,15 @@ platform_check_image() {
 	dwr-512-b|\
 	e1700|\
 	esr-9753|\
-	ew1200|\
 	ex2700|\
-	ex3700|\
 	f7c027|\
 	firewrt|\
 	fonera20n|\
 	freestation5|\
-	gb-pc1|\
 	gl-mt300a|\
 	gl-mt300n|\
 	gl-mt750|\
-	gl-mt300n-v2|\
 	hc5*61|\
-	hc5661a|\
 	hg255d|\
 	hlk-rm04|\
 	hpm|\
@@ -75,8 +71,6 @@ platform_check_image() {
 	jhr-n805r|\
 	jhr-n825r|\
 	jhr-n926r|\
-	k2p|\
-	kn|\
 	kn_rc|\
 	kn_rf|\
 	kng_rc|\
@@ -84,8 +78,7 @@ platform_check_image() {
 	linkits7688d|\
 	m2m|\
 	m3|\
-	m4-4M|\
-	m4-8M|\
+	m4|\
 	mac1200rv2|\
 	microwrt|\
 	miniembplug|\
@@ -99,7 +92,6 @@ platform_check_image() {
 	mpr-a2|\
 	mr-102n|\
 	mt7628|\
-	ip04296|\
 	mzk-750dhp|\
 	mzk-dp150n|\
 	mzk-ex300np|\
@@ -109,26 +101,23 @@ platform_check_image() {
 	nbg-419n|\
 	nbg-419n2|\
 	newifi-d1|\
-	nixcore-x1-8M|\
-	nixcore-x1-16M|\
+	nixcore|\
 	nw718|\
 	omega2|\
 	omega2p|\
 	oy-0001|\
 	pbr-d1|\
+	ip04297|\
 	pbr-m1|\
 	psg1208|\
-	psg1218a|\
-	psg1218b|\
+	psg1218|\
 	psr-680w|\
-	px-4885-4M|\
-	px-4885-8M|\
+	px-4885|\
 	rb750gr3|\
 	re6500|\
 	rp-n53|\
 	rt5350f-olinuxino|\
 	rt5350f-olinuxino-evb|\
-	rt-ac51u|\
 	rt-g32-b1|\
 	rt-n10-plus|\
 	rt-n13u|\
@@ -139,20 +128,16 @@ platform_check_image() {
 	sap-g3200u3|\
 	sk-wb8|\
 	sl-r7205|\
-	tew-638apb-v2|\
 	tew-691gr|\
 	tew-692gr|\
 	tew-714tru|\
 	timecloud|\
 	tiny-ac|\
-	u25awf-h1|\
 	ur-326n4g|\
 	ur-336un|\
 	v22rw-2x2|\
-	vocore-8M|\
-	vocore-16M|\
+	vocore|\
 	vocore2|\
-	vocore2lite|\
 	vr500|\
 	w150m|\
 	w2914nsv2|\
@@ -173,39 +158,30 @@ platform_check_image() {
 	wl-wn575a3|\
 	wli-tx4-ag300n|\
 	wlr-6000|\
-	wmdr-143n|\
 	wmr-300|\
 	wn3000rpv3|\
 	wnce2001|\
 	wndr3700v5|\
-	wr512-3gn-4M|\
-	wr512-3gn-8M|\
+	wr512-3gn|\
 	wr6202|\
 	wrh-300cr|\
 	wrtnode|\
 	wrtnode2r |\
 	wrtnode2p |\
 	wsr-600|\
-	wt1520-4M|\
-	wt1520-8M|\
-	wt3020-4M|\
-	wt3020-8M|\
+	wt1520|\
+	wt3020|\
 	wzr-agl300nh|\
 	x5|\
 	x8|\
 	y1|\
 	y1s|\
-	we1026-5g-16m|\
 	zbt-ape522ii|\
 	zbt-cpe102|\
 	zbt-wa05|\
-	zbt-we1326|\
-	zbt-we2026|\
-	zbt-we826-16M|\
-	zbt-we826-32M|\
+	zbt-we826|\
 	zbt-wg2626|\
-	zbt-wg3526-16M|\
-	zbt-wg3526-32M|\
+	zbt-wg3526|\
 	zbt-wr8305rt|\
 	zte-q7|\
 	youku-yk1)
@@ -232,12 +208,9 @@ platform_check_image() {
 		}
 		return 0
 		;;
-	c20|\
 	c20i|\
 	c50|\
-	mr200|\
-	tl-wr840n-v4|\
-	tl-wr841n-v13)
+	mr200)
 		[ "$magic" != "03000000" ] && {
 			echo "Invalid image type."
 			return 1
@@ -255,31 +228,16 @@ platform_check_image() {
 		}
 		return 0
 		;;
-	hc5962|\
-	mir3g|\
-	r6220)
-		# these boards use metadata images
-		return 0
-		;;
-	re350-v1)
-		[ "$magic" != "01000000" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	ubnt-erx|\
-	ubnt-erx-sfp)
-		nand_do_platform_check "$board" "$1"
-		return $?;
-		;;
-	wcr-1166ds|\
 	wsr-1166)
 		[ "$magic" != "48445230" ] && {
 			echo "Invalid image type."
 			return 1
 		}
 		return 0
+		;;
+	ubnt-erx)
+		nand_do_platform_check "$board" "$1"
+		return $?;
 		;;
 	esac
 
@@ -288,27 +246,29 @@ platform_check_image() {
 }
 
 platform_nand_pre_upgrade() {
-	local board=$(board_name)
+	local board=$(ramips_board_name)
 
 	case "$board" in
-	ubnt-erx|\
-	ubnt-erx-sfp)
+	ubnt-erx)
 		platform_upgrade_ubnt_erx "$ARGV"
 		;;
 	esac
 }
 
-platform_do_upgrade() {
-	local board=$(board_name)
+platform_pre_upgrade() {
+	local board=$(ramips_board_name)
 
 	case "$board" in
-	hc5962|\
-	mir3g|\
-	r6220|\
-	ubnt-erx|\
-	ubnt-erx-sfp)
+    	ubnt-erx)
 		nand_do_upgrade "$ARGV"
 		;;
+	esac
+}
+
+platform_do_upgrade() {
+	local board=$(ramips_board_name)
+
+	case "$board" in
 	*)
 		default_do_upgrade "$ARGV"
 		;;
